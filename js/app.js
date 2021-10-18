96f861ddd670deeef3a0d7547d9ad403d59248a9
mu2ku <gmutuku@gmail.com>
2021-10-18 14:58:41 -0400

    commit new functionality for drawing player's hand

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ let drawBtn = document.getElementById('draw-btn')
 let board = document.querySelector('.board')
 let opponent = document.querySelector('.opponent')
 let player = document.querySelectorAll('.player')
+let playerBoard = document.querySelector('#player')
 let deal = document.querySelector('.deal')
 
 
@@ -28,12 +29,13 @@ player.forEach((element,idx)=>{player[idx].addEventListener('click',test)})
 init()
 
 function init(){
-  deck = ['d0_0','d0_1','d0_2','d0_3','d0_4','d0_5','d0_6','d1_1','d1_2','d1_3','d1_4','d1_5','d1_6','d2_2','d2_3','d2_4','d2_5','d2_6','d3_3','d3_4','d3_5','d3_6','d4_4','d4_5','d4_6','d5_5','d5_6','d6_6'] 
+  deck = ['d0_0','d0_1','d0_2','d0_3','d0_4','d0_5','d0_6','d1_1','d1_2','d1_3','d1_4','d1_5','d1_6','d2_2','d2_3','d2_4','d2_5','d2_6','d3_3','d3_4','d3_5','d3_6','d4_4','d4_5','d4_6','d5_5','d5_6','d6_6']
+  resetBtn.innerText = "Start Game"
 }
 
 function handleClick(){
-  resetBtn.innerText = "Reset Game"
-  if (deck.length > 0){
+  resetBtn.innerText = 'Reset Game'
+  if (deck.length > 14){
     for (let i=0;i<14;i++){
       let randIdx = Math.floor(Math.random()*deck.length)
       let shuffle = deck.splice(randIdx,1)
@@ -44,15 +46,29 @@ function handleClick(){
 }
 
 function render(dominosPicked){
+  //render player hand to board
+  if (playerHand.length === 0){
   playerHand = dominosPicked.splice(0,7)
-  opponentHand = dominosPicked.splice(0,7)
-  console.log(playerHand)
-  console.log(opponentHand)
-  console.log(dominosPicked)
+  for(let i=0;i<playerHand.length;i++){
+    let playerImg = document.createElement("img")
+    playerImg.setAttribute("id","[i]")
+    playerImg.setAttribute("class","player")
+    playerImg.setAttribute("src",`/images/${playerHand[i]}.png`)
+    playerBoard.append(playerImg)
+  }
+}
+  //render opponent hand to board
+  if (opponentHand.length === 0){
+    opponentHand = dominosPicked.splice(0,7)
+  }
 }
 
 function test(evt){
   if(evt.target.id !== "player"){
     evt.stopPropagation(console.log('success'))
   }
+}
+
+function drawTile(){
+  
 }
