2ab7b0095a2f405cef74dc06adf8e28bcd3f753f
mu2ku <gmutuku@gmail.com>
2021-10-18 03:29:21 -0400

    commit basic javascript

---
+++ js/app.js
@@ -0,0 +1,23 @@
+/*----------------- Constants -----------------*/
+
+
+/*------------- Variables (state) -------------*/
+
+let deck = []
+
+/*--------- Cached Element References ---------*/
+
+let resetBtn = document.getElementById('reset-btn')
+let drawBtn = document.getElementById('draw-btn')
+
+
+/*-------------- Event Listeners --------------*/
+
+
+/*----------------- Functions -----------------*/
+
+init()
+
+function init(){
+  deck = ['0_0','0_1','0_2','0_3','0_4','0_5','0_6','1_1','1_2','1_3','1_4','1_5','1_6','2_2','2_3','2_4','2_5','2_6','3_3','3_4','3_5','3_6','4_4','4_5','4_6','5_5','5_6','6_6'] 
+}
